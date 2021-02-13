c6580335512dc2e4d766db8f538f7dbce25b681c
kotbegemot <k0tb9g9m0t@yandex.ru>
2021-02-14 01:30:17 +0300

    fix

--- .ci/run.sh
+++ .ci/run.sh
@@ -13,6 +13,6 @@ fi
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remote add cyberduckninja https://api.bintray.com/conan/cyberduckninja/conan
 #- mkdir build && cd build
-conan install . --build=missing #-s build_type=Release --build=missing #--install-folder=build
+conan install . --build=missing -s compiler.libcxx=libstdc++11 -s build_type=Release # --build=missing #--install-folder=build
 cmake . -DCMAKE_BUILD_TYPE=Release -DHTTP_COMPONENT=ON -DALLOW_EXAMPLES=ON
 cmake --build . -- VERBOSE=1
